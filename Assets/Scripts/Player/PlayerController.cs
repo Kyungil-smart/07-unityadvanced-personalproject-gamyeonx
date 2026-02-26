53e7a4a59e638f7e364d07014c616d2df365b9df
Geunhyung Song <gamyeonx@gmail.com>
2026-02-26 16:12:13 +0900

    [Add] 플레이어 점프 기능 & 애니메이션 추가

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,17 +5,20 @@ using static UnityEngine.Rendering.DebugUI;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] public float _moveSpeed;
-    [SerializeField] public float _jumpHeight;
+    [SerializeField] public float _moveSpeed = 3;
+    [SerializeField] public float _jumpHeight = 7;
 
     [SerializeField] Animator _animator;
 
-    private Rigidbody2D _rigidbody; 
+    private Rigidbody2D _rigidbody;
     private Vector2 _moveInput;
 
     private InputAction _moveAction;
     private InputAction _jumpAction;
     private InputAction _runAction;
+
+    [SerializeField] private LayerMask _jumpCheckLayer;
+    [SerializeField] private float _rayDistance = 1.1f;
 
     private bool _runInput = false;
 
@@ -32,8 +35,8 @@ public class PlayerController : MonoBehaviour
     {
         _moveAction.performed += OnMove;
         _moveAction.canceled += MoveCancel;
-        //_jumpAction.started += OnJump;
-        //_jumpAction.canceled += JumpCancel;
+        _jumpAction.started += OnJump;
+        _jumpAction.canceled += JumpCancel;
         _runAction.started += OnRun;
         _runAction.canceled += RunCancel;
     }
@@ -41,11 +44,12 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         _rigidbody.linearVelocity = new Vector2(_moveInput.x * _moveSpeed, _rigidbody.linearVelocity.y);
+        IsGrounded();
+        Debug.DrawRay(transform.position, Vector2.down * 1.1f, Color.red);
     }
 
     private void Update()
     {
-
         if (_moveInput.x != 0)
         {
             if (_moveInput.x > 0)
@@ -62,8 +66,8 @@ public class PlayerController : MonoBehaviour
     {
         _moveAction.performed -= OnMove;
         _moveAction.canceled -= MoveCancel;
-        //_jumpAction.started -= OnJump;
-        //_jumpAction.canceled -= JumpCancel;
+        _jumpAction.started -= OnJump;
+        _jumpAction.canceled -= JumpCancel;
         _runAction.started -= OnRun;
         _runAction.canceled -= RunCancel;
     }
@@ -78,31 +82,51 @@ public class PlayerController : MonoBehaviour
         _moveInput = Vector2.zero;
     }
 
+    public bool IsGrounded()
+    {
+        return Physics2D.Raycast(transform.position, Vector2.down, _rayDistance, _jumpCheckLayer);
+    }
+
     public void OnJump(InputAction.CallbackContext ctx)
     {
-
+        {
+            if (ctx.started && IsGrounded())
+            {
+                _rigidbody.linearVelocity = new Vector2(_rigidbody.linearVelocity.x, _jumpHeight);
+                _animator.SetTrigger("IsJump");
+            }
+        }
     }
 
     public void JumpCancel(InputAction.CallbackContext ctx)
     {
-
+        if (ctx.canceled && _rigidbody.linearVelocity.y > 0)
+        {
+            _rigidbody.linearVelocity = new Vector2(_rigidbody.linearVelocity.x, _rigidbody.linearVelocity.y * 0.4f);
+        }
     }
 
     public void OnRun(InputAction.CallbackContext ctx)
     {
-        if(ctx.started)
+        if (ctx.started)
         {
             _runInput = true;
             _moveSpeed += 2;
-        }    
+        }
     }
 
     public void RunCancel(InputAction.CallbackContext ctx)
     {
-        if(ctx.canceled)
+        if (ctx.canceled)
         {
             _runInput = false;
             _moveSpeed -= 2;
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, Vector2.down * _rayDistance);
     }
 }
