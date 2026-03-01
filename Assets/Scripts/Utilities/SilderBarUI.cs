7d3334647cf67c4fda1cb46829fd62c82e08d9c9
Geunhyung Song <gamyeonx@gmail.com>
2026-03-01 22:33:23 +0900

    [Edit] HP바 테스트 코드 삭제

--- Assets/Scripts/Utilities/SilderBarUI.cs
+++ Assets/Scripts/Utilities/SilderBarUI.cs
@@ -7,18 +7,6 @@ public class SilderBarUI : MonoBehaviour
     [SerializeField] private Slider _slider;
     [SerializeField] private Image _fillImage;
 
-    private float _maxHP = 100f;
-    private float _currentHP = 100f;
-
-    void Update()
-    {
-        if (Keyboard.current.spaceKey.wasPressedThisFrame)
-        {
-            _currentHP -= 20f;
-            SetHP(_currentHP, _maxHP);
-        }
-    }
-
     public void SetHP(float current, float max)
     {
         float percent = current / max;
