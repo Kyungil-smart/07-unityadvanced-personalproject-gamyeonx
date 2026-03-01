1c80e8c27f957f54c8f0de7af1fc9d25490f8702
Geunhyung Song <gamyeonx@gmail.com>
2026-03-01 15:50:54 +0900

    [Edit] 여러 옵셋 값 변경
    *버니의 경우 벽 한칸 위로 올라올 아이디어로 피직스 마테리얼에 바운스값 추가

--- Assets/Scripts/Monsters/Bunny/AttackAction.cs
+++ Assets/Scripts/Monsters/Bunny/AttackAction.cs
@@ -5,15 +5,26 @@ using Action = Unity.Behavior.Action;
 using Unity.Properties;
 
 [Serializable, GeneratePropertyBag]
-[NodeDescription(name: "Attack", story: "[Self] [Target] [AttackRange]", category: "Action", id: "5920af41a2831e0c1e0683fc65f4b5e2")]
+[NodeDescription(name: "Attack", story: "[Self] [Target] [AttackRange] [DashSpeed] [JumpForce]", category: "Action", id: "5920af41a2831e0c1e0683fc65f4b5e2")]
 public partial class AttackAction : Action
 {
     [SerializeReference] public BlackboardVariable<GameObject> Self;
     [SerializeReference] public BlackboardVariable<GameObject> Target;
     [SerializeReference] public BlackboardVariable<float> AttackRange;
+    [SerializeReference] public BlackboardVariable<float> DashSpeed;
+    [SerializeReference] public BlackboardVariable<float> JumpForce;
+
+    private bool _initialized = false;
+    private Rigidbody2D _rb;
 
     protected override Status OnStart()
     {
+        if (!_initialized)
+        {
+            _rb = Self.Value.GetComponent<Rigidbody2D>();
+            _initialized = true;
+        }
+
         return Status.Running;
     }
 
@@ -23,7 +34,8 @@ public partial class AttackAction : Action
 
         if (distance <= AttackRange.Value)
         {
-          
+
+            return Status.Success; 
         }
 
         return Status.Failure;
